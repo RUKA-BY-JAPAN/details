0bb2a5ae49ab4d5dbf5f3b6c6a6554d326d49b2a
RUKA-BY-JAPAN <rukabottest@gmail.com>
2025-07-29 12:49:41 +0530

    Update details.js

--- details.js
+++ details.js
@@ -5,4 +5,4 @@
     "header" : "const introMessage = `*👋 HELLO ${pushname},*\n\n*╭─「 𝗦𝗢𝗟𝗢 𝗟𝗘𝗩𝗘𝗟𝗜𝗡𝗚 𝗫 」*\n*│⌚ Uptime :* *${runtime(process.uptime())}*\n*│💾 RAM Usage :* *${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} MB / ${Math.round(os.totalmem() / 1024 / 1024)} MB*\n*╰─────────────────────⊷*\n\n*╭──「 𝘚𝘖𝘓𝘖 𝘓𝘌𝘝𝘌𝘓𝘐𝘕𝘎 𝘟 」*\n*│🤖 Bot Name :* 𝘚𝘖𝘓𝘖 𝘓𝘌𝘝𝘌𝘓𝘐𝘕𝘎 𝘟\n*│👑 Owner :* RUKSHAN\n*│🎭 Support :* Legion of Doom\n*│⏰ Time :* ${formattedTime}\n*│📆 Date :* ${formattedDate}\n*│📎 Prefix :* ${prefix}\n*│🧠 Version :* v4.0.0\n*│🔗 GitHub :* No Repo Available\n*╰─────────────────────⊷*`;,
     "channel" : "https://whatsapp.com/channel/0029VbAWWH9BFLgRMCXVlU38",
     "repo" : "not details"
-      }
+ }
